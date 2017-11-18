9e897c676de42d4f354c68fe1dc3a0cb75478986
Miguel Lattuada <mlattuada@altimetrik.com>
2017-11-18 18:52:55 -0300

    Added free shipping image

--- src/client/app/components/shared/product-row/product-row.template.jsx
+++ src/client/app/components/shared/product-row/product-row.template.jsx
@@ -8,7 +8,10 @@ export default function () {
                 <img src={this.props.product.picture} alt={this.props.product.title} />
             </div>
             <div className="product-row-product-content">
-                <h3>{this.props.product.normalizedPrice}</h3>
+                <h3>
+                    {this.props.product.normalizedPrice}
+                    {this.props.product.free_shipping ? (<small><img className="free-shipping" src="public/assets/ic_shipping@2x.png" alt="Envio gratis" /></small>) : undefined}
+                </h3>
                 <p>{this.props.product.title}</p>
             </div>
             <div className="product-row-product-place">
